855c1ce57851672335a79c1481e9f16824fe5861
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-11-09 00:58:26 +0100

    Fragment passenger account
    Finished fragment with basic information about passenger's account.

--- app/src/main/java/com/example/ubermobileapp/PassengerAccountActivity.java
+++ app/src/main/java/com/example/ubermobileapp/PassengerAccountActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+
 public class PassengerAccountActivity extends AppCompatActivity {
 
     @Override

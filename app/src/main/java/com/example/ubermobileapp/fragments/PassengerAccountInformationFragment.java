bdbbb1276e727147d333f1244b0a7f5a83beca81
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-11-09 20:59:10 +0100

    Added second fragment and worked on first

--- app/src/main/java/com/example/ubermobileapp/fragments/PassengerAccountInformationFragment.java
+++ app/src/main/java/com/example/ubermobileapp/fragments/PassengerAccountInformationFragment.java
@@ -1,4 +1,4 @@
-package com.example.ubermobileapp;
+package com.example.ubermobileapp.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.ubermobileapp.R;
 
 /**
  * A simple {@link Fragment} subclass.

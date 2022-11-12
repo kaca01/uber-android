d02f321eb0e436a65a868ea1a6d5460598d7fa2c
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-11-12 17:16:31 +0100

    Added dialog with credit card information
    Also changed passenger account activity

--- app/src/main/java/com/example/ubermobileapp/fragments/PassengerAccountOptionsFragment.java
+++ app/src/main/java/com/example/ubermobileapp/fragments/PassengerAccountOptionsFragment.java
@@ -1,16 +1,23 @@
 package com.example.ubermobileapp.fragments;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ubermobileapp.R;
 
 public class PassengerAccountOptionsFragment extends Fragment {
+    AlertDialog alertDialog;
 
     public PassengerAccountOptionsFragment() {
         // Required empty public constructor
@@ -29,7 +36,36 @@ public class PassengerAccountOptionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_passenger_account_options, container,
+        View view =  inflater.inflate(R.layout.fragment_passenger_account_options, container,
                 false);
+
+        Button creditCard = view.findViewById(R.id.creditCardBtn);
+        creditCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(requireActivity());
+
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                View newView = inflater.inflate(R.layout.fragment_credit_card, null);
+
+                dialog.setView(newView)
+                        .setTitle("Credit Card")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                alertDialog = dialog.create();
+                alertDialog.show();
+            }
+        });
+
+        return view;
     }
 }

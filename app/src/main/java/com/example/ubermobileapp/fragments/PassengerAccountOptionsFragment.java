53a1b7c51a9291d7f65e3390f45932127e918b05
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-11-12 17:45:33 +0100

    Deleted unused imports

--- app/src/main/java/com/example/ubermobileapp/fragments/PassengerAccountOptionsFragment.java
+++ app/src/main/java/com/example/ubermobileapp/fragments/PassengerAccountOptionsFragment.java
@@ -1,18 +1,15 @@
 package com.example.ubermobileapp.fragments;
 
 import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.ubermobileapp.R;
 

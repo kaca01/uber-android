b8d6fdce173dc5695e7306d9373a964c79862d7f
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-01-10 03:45:09 +0100

    Implemented timer
    Long click for reset of timer.

--- app/src/main/java/com/example/ubermobileapp/activities/home/DriverMainActivity.java
+++ app/src/main/java/com/example/ubermobileapp/activities/home/DriverMainActivity.java
@@ -6,8 +6,10 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.ubermobileapp.R;
 import com.example.ubermobileapp.activities.account.DriverAccountActivity;
@@ -20,8 +22,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.Locale;
+
 public class DriverMainActivity extends AppCompatActivity {
     private boolean play = false;
+
+    private int seconds = 0;
+
+    // Is the stopwatch running?
+    private boolean running;
+
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +64,68 @@ public class DriverMainActivity extends AppCompatActivity {
             }
         });
 
+        writeOnClickListeners();
+
+        if (savedInstanceState != null) {
+
+            // Get the previous state of the stopwatch
+            // if the activity has been
+            // destroyed and recreated.
+            seconds
+                    = savedInstanceState
+                    .getInt("seconds");
+            running
+                    = savedInstanceState
+                    .getBoolean("running");
+            wasRunning
+                    = savedInstanceState
+                    .getBoolean("wasRunning");
+        }
+        runTimer();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
+
+    @Override
+    public void onSaveInstanceState(
+            Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState
+                .putInt("seconds", seconds);
+        savedInstanceState
+                .putBoolean("running", running);
+        savedInstanceState
+                .putBoolean("wasRunning", wasRunning);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    // If the activity is resumed,
+    // start the stopwatch
+    // again if it was running previously.
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    private void writeOnClickListeners() {
         CardView passengers = findViewById(R.id.firstCard);
         passengers.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,10 +140,21 @@ public class DriverMainActivity extends AppCompatActivity {
                 if (!play) {
                     play = true;
                     drawRoute();
+                    onClickStart();
                 } else {
                     play = false;
-                    backToCurrentLocation();
+                    onClickStop();
                 }
+            }
+        });
+
+        start_pause.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                play = false;
+                backToCurrentLocation();
+                onClickReset();
+                return true;
             }
         });
 
@@ -81,15 +165,73 @@ public class DriverMainActivity extends AppCompatActivity {
 
             }
         });
-
     }
 
-    @Override
-    public void onBackPressed() {
-        Intent a = new Intent(Intent.ACTION_MAIN);
-        a.addCategory(Intent.CATEGORY_HOME);
-        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(a);
+    public void onClickStart()
+    {
+        running = true;
+    }
+
+    public void onClickStop()
+    {
+        running = false;
+    }
+
+    public void onClickReset()
+    {
+        running = false;
+        seconds = 0;
+    }
+
+    // Sets the NUmber of seconds on the timer.
+    // The runTimer() method uses a Handler
+    // to increment the seconds and
+    // update the text view.
+    private void runTimer()
+    {
+        final TextView timeView
+                = (TextView)findViewById(
+                R.id.timer);
+
+        final Handler handler
+                = new Handler();
+
+        // Call the post() method,
+        // passing in a new Runnable.
+        // The post() method processes
+        // code without a delay,
+        // so the code in the Runnable
+        // will run almost immediately.
+        handler.post(new Runnable() {
+            @Override
+
+            public void run()
+            {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                // Format the seconds into hours, minutes,
+                // and seconds.
+                String time
+                        = String
+                        .format(Locale.getDefault(),
+                                "%d:%02d:%02d", hours,
+                                minutes, secs);
+
+                timeView.setText(time);
+
+                // If running is true, increment the
+                // seconds variable.
+                if (running) {
+                    seconds++;
+                }
+
+                // Post the code again
+                // with a delay of 1 second.
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 
     private void drawRoute() {

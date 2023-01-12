a6752fecdbdec097c1e0b1d1ba1c6eb95e222bab
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-01-12 18:12:38 +0100

    Created new activity for passenger current ride

--- app/src/main/java/com/example/ubermobileapp/activities/home/PassengerCurrentRideActivity.java
+++ app/src/main/java/com/example/ubermobileapp/activities/home/PassengerCurrentRideActivity.java
@@ -14,21 +14,22 @@ import android.view.View;
 
 import com.example.ubermobileapp.R;
 import com.example.ubermobileapp.activities.account.PassengerAccountActivity;
-import com.example.ubermobileapp.activities.inbox.PassengerInboxActivity;
 import com.example.ubermobileapp.activities.history.PassengerRideHistoryActivity;
+import com.example.ubermobileapp.activities.inbox.PassengerInboxActivity;
 import com.example.ubermobileapp.fragments.home.MapFragment;
 import com.example.ubermobileapp.tools.FragmentTransition;
 import com.example.ubermobileapp.tools.Timer;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class PassengerMainActivity extends AppCompatActivity {
+public class PassengerCurrentRideActivity extends AppCompatActivity {
+    AlertDialog alertDialog;
+    Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_passenger_main);
-
+        setContentView(R.layout.activity_passenger_current_ride);
         BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
         navigation.setSelectedItemId(R.id.page_map);
         navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -38,21 +39,23 @@ public class PassengerMainActivity extends AppCompatActivity {
                     case R.id.page_map:
                         return true;
                     case R.id.page_history:
-                        startActivity(new Intent(PassengerMainActivity.this, PassengerRideHistoryActivity.class));
+                        startActivity(new Intent(PassengerCurrentRideActivity.this, PassengerRideHistoryActivity.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.page_inbox:
-                        startActivity(new Intent(PassengerMainActivity.this, PassengerInboxActivity.class));
+                        startActivity(new Intent(PassengerCurrentRideActivity.this, PassengerInboxActivity.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.page_account:
-                        startActivity(new Intent(PassengerMainActivity.this, PassengerAccountActivity.class));
+                        startActivity(new Intent(PassengerCurrentRideActivity.this, PassengerAccountActivity.class));
                         overridePendingTransition(0,0);
                         return true;
                 }
                 return false;
             }
         });
+
+        writeOnClickListeners();
     }
 
     @Override
@@ -61,5 +64,47 @@ public class PassengerMainActivity extends AppCompatActivity {
         a.addCategory(Intent.CATEGORY_HOME);
         a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(a);
+    }
+
+    private void createDriverDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(PassengerCurrentRideActivity.this);
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View newView = inflater.inflate(R.layout.fragment_passengers_info, null);
+
+        dialog.setView(newView)
+                .setTitle("Driver Info")
+                .setCancelable(true)
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        alertDialog = dialog.create();
+        alertDialog.show();
+    }
+
+    private void writeOnClickListeners() {
+        CardView passengers = findViewById(R.id.firstCard);
+        passengers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createDriverDialog();
+            }
+        });
+
+        CardView panic = findViewById(R.id.secondCard);
+        panic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO : implement sending notification
+                backToCurrentLocation();
+                timer.onClickReset();
+            }
+        });
+    }
+
+    private void backToCurrentLocation() {
+        FragmentTransition.to(MapFragment.newInstance(), this, false);
     }
 }

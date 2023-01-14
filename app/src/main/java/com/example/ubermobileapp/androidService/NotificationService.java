2470b90f30c7713210c6309145a8af66d1e49153
savic-a <anastasijas557@gmail.com>
2023-01-14 23:13:39 +0100

    Changed notification service

--- app/src/main/java/com/example/ubermobileapp/androidService/NotificationService.java
+++ app/src/main/java/com/example/ubermobileapp/androidService/NotificationService.java
@@ -7,70 +7,23 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
 import com.example.ubermobileapp.R;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NotificationService extends Service {
 
     public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
     private final static String default_notification_channel_id = "default" ;
-    Timer timer;
-    TimerTask timerTask;
-    String TAG = "Timers";
-    int Your_X_SECS = 5;
 
-    @Override
-    public int onStartCommand (Intent intent, int flags ,int startId) {
-        Log.e(TAG, "onStartCommand");
-        super.onStartCommand(intent, flags, startId);
-        startTimer();
-        return START_STICKY;
-    }
-
-    @Override
-    public void onCreate() {
-        Log.e(TAG, "onCreate");
-    }
-
-    @Override
-    public void onDestroy() {
-        Log.e(TAG , "onDestroy") ;
-        stopTimerTask() ;
-        super.onDestroy() ;
-    }
-
-    // we are going to use a handler to be able to run in our TimerTask
-    final Handler handler = new Handler() ;
-    public void startTimer() {
-        timer = new Timer() ;
-        initializeTimerTask() ;
-        timer.schedule(timerTask, 15000, Your_X_SECS * 1000000 );
-    }
-
-    public void stopTimerTask() {
-        if ( timer != null ) {
-            timer.cancel() ;
-            timer = null;
-        }
-    }
-
-    public void initializeTimerTask() {
-        timerTask = new TimerTask() {
-            public void run () {
-                handler .post( new Runnable() {
-                    public void run () {
-                        createNotification() ;
-                    }
-                });
-            }
-        };
-    }
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+    Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -91,5 +44,22 @@ public class NotificationService extends Service {
             mNotificationManager.createNotificationChannel(notificationChannel) ;
         }
         mNotificationManager.notify(1, mBuilder.build()) ;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("REZ", "SyncService onStartCommand");
+        executor.execute(() -> {
+            //Background work here
+            Log.i("REZ", "Background work here");
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            handler.post(this::createNotification);
+        });
+        stopSelf();
+        return START_NOT_STICKY;
     }
 }

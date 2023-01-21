8052749755ddce57e3155c190d13a0a5820cf37e
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-01-21 05:05:19 +0100

    Added click listeners

--- app/src/main/java/com/example/ubermobileapp/fragments/account/AccountOptionsFragment.java
+++ app/src/main/java/com/example/ubermobileapp/fragments/account/AccountOptionsFragment.java
@@ -1,14 +1,25 @@
 package com.example.ubermobileapp.fragments.account;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.ubermobileapp.R;
+import com.example.ubermobileapp.activities.reports.PassengerReportsActivity;
+import com.example.ubermobileapp.models.login.User;
+import com.example.ubermobileapp.services.utils.AuthService;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +27,12 @@ import com.example.ubermobileapp.R;
  * create an instance of this fragment.
  */
 public class AccountOptionsFragment extends Fragment {
+    AlertDialog startDateDialog;
+    AlertDialog endDateDialog;
+    AlertDialog alertDialog;
+    private View view;
+    int SELECT_IMAGE_CODE = 1;
+    ImageView imgGallery;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +78,138 @@ public class AccountOptionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_options, container, false);
+        view =  inflater.inflate(R.layout.fragment_account_options, container, false);
+
+        CardView editData = view.findViewById(R.id.firstCard);
+        editData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createEditDialog();
+            }
+        });
+
+        CardView reports = view.findViewById(R.id.secondCard);
+        reports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createStartDateDialog();
+            }
+        });
+
+        return view;
+    }
+
+
+    protected void createStartDateDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(requireActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View newView = inflater.inflate(R.layout.fragment_start_date, null);
+
+        dialog.setView(newView)
+                .setTitle("Reports")
+                .setCancelable(false)
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        createEndDateDialog();
+                    }
+                })
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        startDateDialog = dialog.create();
+        startDateDialog.show();
+    }
+
+
+    protected void createEndDateDialog() {
+        startDateDialog.cancel();
+        AlertDialog.Builder dialog = new AlertDialog.Builder(requireActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View newView = inflater.inflate(R.layout.fragment_end_date, null);
+
+        dialog.setView(newView)
+                .setTitle("Reports")
+                .setCancelable(false)
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(getActivity(), PassengerReportsActivity.class));
+                    }
+                })
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        endDateDialog = dialog.create();
+        endDateDialog.show();
+    }
+
+    protected void createEditDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(requireActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View newView = inflater.inflate(R.layout.fragment_edit_account_information, null);
+        setEditAccountData(newView);
+
+        Button button = newView.findViewById(R.id.btnChangePic);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Title"), SELECT_IMAGE_CODE);
+            }
+        });
+
+        imgGallery = view.findViewById(R.id.profilePicture);
+
+        dialog.setView(newView)
+                .setTitle("Edit account information")
+                .setCancelable(false)
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        alertDialog = dialog.create();
+        alertDialog.show();
+    }
+
+    private void setEditAccountData(View newView) {
+        User passenger = AuthService.getCurrentUser();
+
+        TextView name = newView.findViewById(R.id.inputName);
+        TextView lastName = newView.findViewById(R.id.inputSurname);
+        TextView postalAddress = newView.findViewById(R.id.postalAdressInput);
+        TextView email = newView.findViewById(R.id.inputEmail);
+        TextView phone = newView.findViewById(R.id.inputPhone);
+        // TODO : check how user can change password
+        TextView password = newView.findViewById(R.id.passwordInput);
+        TextView passwordAgain = newView.findViewById(R.id.passwordInputAgain);
+
+        name.setText(passenger.getName());
+        lastName.setText(passenger.getSurname());
+        postalAddress.setText(passenger.getAddress());
+        email.setText(passenger.getEmail());
+        phone.setText(passenger.getTelephoneNumber());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==1) {
+            if (data != null) imgGallery.setImageURI(data.getData());
+        }
     }
 }
--- app/src/main/java/com/example/ubermobileapp/fragments/account/PassengerAccountInformationFragment.java
+++ app/src/main/java/com/example/ubermobileapp/fragments/account/PassengerAccountInformationFragment.java
@@ -26,10 +26,7 @@ import com.example.ubermobileapp.services.utils.AuthService;
  * create an instance of this fragment.
  */
 public class PassengerAccountInformationFragment extends Fragment {
-    AlertDialog alertDialog;
-    private View view;
-    int SELECT_IMAGE_CODE = 1;
-    ImageView imgGallery;
+
 
     public PassengerAccountInformationFragment() {
         // Required empty public constructor
@@ -55,18 +52,8 @@ public class PassengerAccountInformationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view =  inflater.inflate(R.layout.fragment_passenger_account_information, container,
+        View view =  inflater.inflate(R.layout.fragment_passenger_account_information, container,
                 false);
-
-        setAccountData();
-
-//        ImageButton edit = view.findViewById(R.id.buttonEdit);
-//        edit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                createEditDialog();
-//            }
-//        });
 
         return view;
     }
@@ -83,87 +70,5 @@ public class PassengerAccountInformationFragment extends Fragment {
                 startActivity(intent);
             }
         });*/
-    }
-
-    protected void createEditDialog() {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(requireActivity());
-
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View newView = inflater.inflate(R.layout.fragment_edit_account_information, null);
-        setEditAccountData(newView);
-
-        Button button = newView.findViewById(R.id.btnChangePic);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Title"), SELECT_IMAGE_CODE);
-            }
-        });
-
-        imgGallery = view.findViewById(R.id.profilePicture);
-
-        dialog.setView(newView)
-                .setTitle("Edit account information")
-                .setCancelable(false)
-                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                })
-                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-        alertDialog = dialog.create();
-        alertDialog.show();
-    }
-
-    private void setAccountData() {
-        User passenger = AuthService.getCurrentUser();
-
-//        TextView name = view.findViewById(R.id.name);
-//        TextView postalAddress = view.findViewById(R.id.address);
-//        TextView email = view.findViewById(R.id.email);
-//        TextView phone = view.findViewById(R.id.phoneNumber);
-//
-//        String firstAndLastName = passenger.getName() + " " + passenger.getSurname();
-//        name.setText(firstAndLastName);
-//        postalAddress.setText(passenger.getAddress());
-//        email.setText(passenger.getEmail());
-//        phone.setText(passenger.getTelephoneNumber());
-    }
-
-    private void setEditAccountData(View newView) {
-        User passenger = AuthService.getCurrentUser();
-
-        TextView name = newView.findViewById(R.id.inputName);
-        TextView lastName = newView.findViewById(R.id.inputSurname);
-        TextView postalAddress = newView.findViewById(R.id.postalAdressInput);
-        TextView email = newView.findViewById(R.id.inputEmail);
-        TextView phone = newView.findViewById(R.id.inputPhone);
-        // TODO : check how user can change password
-        TextView password = newView.findViewById(R.id.passwordInput);
-        TextView passwordAgain = newView.findViewById(R.id.passwordInputAgain);
-
-        name.setText(passenger.getName());
-        lastName.setText(passenger.getSurname());
-        postalAddress.setText(passenger.getAddress());
-        email.setText(passenger.getEmail());
-        phone.setText(passenger.getTelephoneNumber());
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode==1) {
-            assert data != null;
-            imgGallery.setImageURI(data.getData());
-
-        }
     }
 }

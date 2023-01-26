ab13bf129bda03c2c2814d529a7ac31c657c89d6
Hristina Adamovic <hristinacina@gmail.com>
2023-01-26 02:28:12 +0100

    changed driver account view

--- app/src/main/java/com/example/ubermobileapp/activities/account/DriverAccountActivity.java
+++ app/src/main/java/com/example/ubermobileapp/activities/account/DriverAccountActivity.java
@@ -1,16 +1,24 @@
 package com.example.ubermobileapp.activities.account;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.ubermobileapp.R;
 import com.example.ubermobileapp.activities.inbox.DriverInboxActivity;
 import com.example.ubermobileapp.activities.history.DriverRideHistoryActivity;
 import com.example.ubermobileapp.activities.home.DriverMainActivity;
+import com.example.ubermobileapp.activities.startup.UserLoginActivity;
+import com.example.ubermobileapp.services.utils.AuthService;
+import com.example.ubermobileapp.tools.Timer;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -20,6 +28,16 @@ public class DriverAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_account);
+
+        TextView logOut = findViewById(R.id.logOut);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthService.logout();
+                Timer.setInstance();
+                startActivity(new Intent(DriverAccountActivity.this, UserLoginActivity.class));
+            }
+        });
 
         BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
         navigation.setSelectedItemId(R.id.page_account);

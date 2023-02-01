52248b00552a5ff83ad27e283bce4799555f9e63
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-02-01 19:01:02 +0100

    Fixes

--- app/src/main/java/com/example/ubermobileapp/services/utils/ApiUtils.java
+++ app/src/main/java/com/example/ubermobileapp/services/utils/ApiUtils.java
@@ -9,7 +9,7 @@ import com.example.ubermobileapp.services.IUserService;
 
 public class ApiUtils {
 
-    public static final String BASE_URL = "http://192.168.0.34:8081/api/";
+    public static final String BASE_URL = "http://192.168.0.102:8081/api/";
 
     public static IUserService getUserService(){
         return RetrofitClient.getClient(BASE_URL).create(IUserService.class);
